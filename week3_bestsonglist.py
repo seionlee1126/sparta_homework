4bb3ba68a34f1cf467957324719e05cfc204247d
seionlee1126 <67576951+seionlee1126@users.noreply.github.com>
2020-08-01 00:17:54 +0900

    w3_assignment

---
+++ week3_bestsonglist.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713',headers=headers)
+
+songs = db.songs.find()
+
+for song in songs:
+    print(song['rank'], song['title'], song['singer'])
